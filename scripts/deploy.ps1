4c9964d66ccd4e78e2687b185339610a82dbec3d
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2020-11-06 10:19:53 -0500

    Updated deployment script

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,8 +1,10 @@
 # Because plots are uniquely determined by some hash by pandoc-plot,
 # images can build up on the remote server
 # We clean out all files before transfer
+# Note that after all files are deleted, the siwicklab website (from ~/website) needs
+# to be re-transferred
 echo "Delete current website (skip with CTRL-C):"
-ssh decotret@gollum.physics.mcgill.ca 'rm -r -f /common/WWW/decotret/*';
+ssh decotret@gollum.physics.mcgill.ca "rm -r -f /common/WWW/decotret/*; rsync -a 'website/' /WWW/decotret/siwicklab";
 
 echo "Transfer last-recently built website:"
 scp -Cr ./decotret decotret@gollum.physics.mcgill.ca:/common/WWW/
