b4663abbbd12df973777af1c5400b2a00508ba14
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2020-11-18 13:41:27 -0500

    Minifying of HTML files as well

--- tools/src/minifier.rs
+++ tools/src/minifier.rs
@@ -1,10 +1,14 @@
 extern crate clap;
 extern crate minifier;
+extern crate minify;
 
 use clap::{App, Arg};
 use std::collections::HashSet;
+use std::ffi::OsStr;
+use minifier::css;
+use minify::html;
 
-// Recursively find all HTML files in a directory `dir`
+// Recursively find all files in a directory `dir`
 fn visit_dir_recursively(dir: &std::path::PathBuf) -> HashSet<std::path::PathBuf> {
     let mut files = HashSet::new();
 
@@ -22,11 +26,18 @@ fn visit_dir_recursively(dir: &std::path::PathBuf) -> HashSet<std::path::PathBuf
     return files;
 }
 
-// Minify CSS in-place
+// Minify CSS and HTML in-place
+// Any other type of file is left unchanged.
 fn minify(path: &std::path::PathBuf) -> std::io::Result<()> {
-    let css = std::fs::read_to_string(path)?;
-    let minified = minifier::css::minify(&css);
-    std::fs::write(&path, minified.unwrap())?;
+    let content = std::fs::read_to_string(path)?;
+
+    let minified = match path.extension().and_then(OsStr::to_str) {
+        Some("css") => Some(css::minify(&content).unwrap()),
+        Some("html") => Some(html::minify(&content)), 
+        _other => None ,
+    };
+    
+    minified.map(|m| std::fs::write(&path, m));
     Ok(())
 }
 
@@ -34,7 +45,7 @@ fn main () {
     let matches = App::new("minifier")
         .version("v1.0")
         .author("Laurent P. René de Cotret")
-        .about("Minify CSS files")
+        .about("Minify CSS and HTML files")
         .arg(
             Arg::with_name("INPUT")
                 .help("File / directory to check")
@@ -59,8 +70,11 @@ fn main () {
             f
         };
         
-        for path in files.iter().filter(|p| p.extension() == Some(std::ffi::OsStr::new("css"))) {
-            minify(path);
-            println!("Minifying {}", path.display());
+        let allowed_ext = vec![Some("css"), Some("html")];
+        for path in files.iter() {
+            if allowed_ext.contains(&path.extension().and_then(OsStr::to_str)) {
+                minify(path);
+                println!("Minifying {}", path.display());
+            }
         }
 }
